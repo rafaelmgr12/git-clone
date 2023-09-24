b9918991ad8b983a3e0d333685815a397568fc23
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2023-09-23 23:16:54 -0300

    [feat] add main

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/rafaelmgr12/git-clone/internal/commands"
+	"github.com/spf13/cobra"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	var rootCmd = &cobra.Command{Use: "fit"}
+
+	rootCmd.AddCommand(commands.InitCmd)
+	rootCmd.AddCommand(commands.AddCmd)
+	rootCmd.AddCommand(commands.LogCmd)
+	rootCmd.AddCommand(commands.StatusCmd)
+	rootCmd.AddCommand(commands.CommitCmd)
+
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
